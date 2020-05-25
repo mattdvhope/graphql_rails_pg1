bfa65f8896c353d5895360e1f56edc745c8fac35
mattdvhope <mattagape@me.com>
2020-05-25 18:47:38 +0700

    Added User

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,8 +1,9 @@
-class Types::UserType < Types::BaseObject
-  graphql_name "User"
+module Types
+  class UserType < Types::BaseObject
+    graphql_name "User"
 
-  field :id, ID, null: false
-  field :name, String, null: false
-  field :picture, String, null: true
-
+    field :id, ID, null: false
+    field :name, String, null: false
+    field :picture, String, null: false
+  end
 end
